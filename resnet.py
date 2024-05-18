db5e46c4ea00405739370bb11be03c3a5a5163af
Evan Chisholm <elchisholm248@gmail.com>
2024-05-17 21:01:11 -0400

    fixed some bugs in resblock implementation

--- resnet.py
+++ resnet.py
@@ -3,17 +3,18 @@ import torch.nn as nn
 
 class ResBlock(nn.Module):
     def __init__(self, in_channels=3, out_channels=3, kernel_size=3, stride=1):
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=1)
+        super(ResBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, padding=1, stride=stride, bias=False)
         self.bn1 = nn.BatchNorm2d(num_features=out_channels)
         self.relu1 = nn.ReLU()
 
-        self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(num_features=out_channels)
         self.relu2 = nn.ReLU()
 
         if stride != 1 or in_channels != out_channels:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.Conv2d(in_channels=in_channels, out_channels=out_channels, padding=0, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(num_features=out_channels)
             )
         else:
@@ -22,24 +23,27 @@ class ResBlock(nn.Module):
         self.relu3 = nn.ReLU()
         
     def forward(self, x):
+        shortcut = self.shortcut(x)
         out = self.relu1(self.bn1(self.conv1(x)))
         out = self.relu2(self.bn2(self.conv2(out)))
-        out = out + self.shortcut(x)
+
+        out = out + shortcut
     
         return self.relu3(out)
 
 class ResNet(nn.Module):
     def __init__(self):
+        super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1)
         self.bn1 = nn.BatchNorm2d(64)
         self.pool1 = nn.MaxPool2d(kernel_size=2)
         
-        self.res_layers = nn.Sequential([
-            ResBlock(64, 64, 2, 1),
-            ResBlock(64, 128, 2, 1),
-            ResBlock(128, 256, 2, 1),
-            ResBlock(256, 512, 2, 1),
-        ])
+        self.res_layers = nn.Sequential(
+            ResBlock(64, 64, 3, 1),
+            ResBlock(64, 128, 3, 1),
+            ResBlock(128, 256, 3, 1),
+            ResBlock(256, 512, 3, 1),
+        )
 
         self.gap = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(in_features=512, out_features=10)
